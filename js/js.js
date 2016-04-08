f7f2879437068190f78883387e1ed74328cd7f42
Arlen Burton <burton.arlen.1990@gmail.com>
2016-04-08 11:31:22 -0700

    brush up the html to match the selection process

--- js/js.js
+++ js/js.js
@@ -7,34 +7,22 @@ function Pizza(){
   this.sauce = [];
   this.amount = [];
 }
-function Toppings(pepperoni, chicken, bacon, mushrooms, olives, onions, peppers, pineapple, soylentGreen){
-  this.pepperoni = pepperoni;
-  this.chicken = chicken;
-  this.bacon = bacon;
-  this.mushrooms = mushrooms;
-  this.olives = olives;
-  this.onions = onions;
-  this.peppers = peppers;
-  this.pineapple = pineapple;
-  this.soylentGreen = people;
+function Toppings(){
+  this.topping = ["pepperoni", "chicken", "bacon", "mushrooms", "olives", "onions", "peppers", "pineapple", "soylentGreen"];
 }
-function Crust(regular, thin, deep, squarePan){
-  this.regular = regular;
-  this.thin = thin;
-  this.deep = deep;
-  this.squarePan = squarePan;
+function Crust(){
+  this.crust = ["regular", "thin", "deep", "squarePan"];
 }
 function Sauce(pizzaSauce, garlicWhite, bbq){
-  this.pizzaSauce = standard;
-  this.garlicWhite = whiteSauce;
-  this.bbq = barbeque;
+  this.sauce = ["pizzaSauce", "garlicWhite", "bbq"];
 }
-function Amount(small, medium, large, absurd){
-  this.small = sm;
-  this.medium = md;
-  this.large = lg;
-  this.absurd = xl;
+function Amount(){
+  this.amount = ["small", "medium", "large", "absurd"];
 }
 Pizza.prototype.finalOrder = function(){
   return "You ordered " + quantity + " " + this.ammount + " " + this.crust + " pizza(s) with " + this.sauce + ", and " + this.topping;
 }
+
+
+
+//user
